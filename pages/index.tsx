d773cae084ee93d22a996bfc10ae7d609b5414fc
Roman Decker <Roman.Decker@zooplus.com>
2022-04-27 13:48:19 +0200

    Showcase how some code is not tracked with coverage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from "next";
+import { foo } from "../no-coverage-here/some-file";
 
-export default function Home() {
+export interface HomeProps {
+  foo: string;
+}
+
+export default function Home({ foo }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main} data-testid="landing_page">
         <h1 className={styles.title}>
-          Welcome to the TypeScript <a href="https://nextjs.org">Next.js</a>{' '}
+          Welcome to the TypeScript <a href="https://nextjs.org">Next.js</a>{" "}
           Cypress example.
         </h1>
 
@@ -21,8 +27,13 @@ export default function Home() {
             <a data-testid="about_page_link">Try going to about page...</a>
           </Link>
           <>(you can't lol, idk)</>
+          {foo}
         </div>
       </main>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  return { props: { foo: foo() } };
+};
